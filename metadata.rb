8cf78040fe4ce4f32831db3b5eb9e0d4f11d5756
Norman Joyner <norman.joyner@gmail.com>
2014-06-24 23:19:07 -0400

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'norman.joyner@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures xen'
 long_description 'A chef cookbook for setting up xen and managing guests'
-version          '0.1.0'
+version          '0.1.1'
 
 supports "ubuntu"
